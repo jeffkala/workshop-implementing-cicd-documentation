553beedb8f271c4d2d2e32f1f19815a537a39916
Jeff Kala <jeff.l.kala@gmail.com>
2024-10-28 13:46:45 -0500

    more prep work for full cicd lab

--- postCreate.sh
+++ postCreate.sh
@@ -6,5 +6,5 @@ curl -sL https://containerlab.dev/setup | sudo -E bash -s "all"
 # install tools
 sudo apt install -y iputils-ping net-tools openssh-client python3-pip
 # install gitlab-runner
-curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-sudo apt-get install gitlab-runner
+# curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+# sudo apt-get install gitlab-runner
